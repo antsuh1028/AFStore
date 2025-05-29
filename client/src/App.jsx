bc787843859f4c1c6294451d5b86bd4fa3fa9747
Shawn Liu <xiangjl4@uci.edu>
2025-05-29 14:29:41 -0700

    additional changes to signup & password recovery

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,17 +5,28 @@ import HomePage from "./pages/Home";
 import ContactPage from "./pages/Contact";
 import TermsAndPolicies from "./pages/Terms";
 import Login from "./pages/Login";
-import {PackingPage, FAQPage, B2BPage, DealPage, HowToOrderPage, MarinatedPage} from "./pages/wholesale"
+import Signup from "./pages/Signup";
+import ForgotPassword from "./pages/ForgotPassword";
+import ResetPassword from "./pages/ResetPassword";
+import AgreementGate from "./pages/AgreementGate";
+import {
+  PackingPage,
+  FAQPage,
+  B2BPage,
+  DealPage,
+  HowToOrderPage,
+  MarinatedPage,
+} from "./pages/wholesale";
 import theme from "./theme";
-import "./fonts.css"; 
+import "./fonts.css";
 
 function App() {
   return (
     <ChakraProvider theme={theme}>
       <BrowserRouter>
-        <Box minH="100vh" bg="#f9f9f9">
+        <Box minH="100vh" display="flex" flexDirection="column" bg="#f9f9f9">
           {/* <Navbar /> */}
-          <Box as="main">
+          <Box as="main" flex="1">
             <Routes>
               <Route path="/" element={<HomePage />} />
               <Route path="/contact" element={<ContactPage />} />
@@ -26,9 +37,11 @@ function App() {
               <Route path="/wholesale/deal" element={<DealPage />} />
               <Route path="/wholesale/marinated" element={<MarinatedPage />} />
               <Route path="/terms-and-policies" element={<TermsAndPolicies />} />
-              <Route path="/login" element={<Login />} /> 
-
-              {/* <Route path="/contact" element={<ContactPage />} /> */}
+              <Route path="/login" element={<Login />} />
+              <Route path="/signup" element={<Signup />} />
+              <Route path="/signup/agreements" element={<AgreementGate />} />
+              <Route path="/forgot-password" element={<ForgotPassword />} />
+              <Route path="/reset-password" element={<ResetPassword />} />
               {/* Add more routes as needed */}
             </Routes>
           </Box>
